b0ecd2dec68f98d2cef6f463ed36bca1eebc5ba5
Veronica Bustabad <vbustabad@gmail.com>
2018-09-20 17:32:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function createForm() {
   const template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   document.getElementsByTagName("main")[0].innerHTML += template({'onSubmit': 'createRecipe()'});
 }
-// 
+//
 // function handlebarsRegistration() {
 //   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
 //   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
-// 
+//
 //   Handlebars.registerHelper('displayIngredient', function(ingredient) {
 //     return new Handlebars.SafeString('<li>' + ingredient + '</li>');
 //   })
